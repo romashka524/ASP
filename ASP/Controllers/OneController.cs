42df159e30721dadb28f54159d75886f993d941b
Ushakova Nadezhda <romashe4_ka93@mail.ru>
2015-03-16 23:38:23 +0500

    Asp

---
+++ ASP/Controllers/OneController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.Controllers
+{
+    public class OneController : Controller
+    {
+        //
+        // GET: /One/
+        public ActionResult View1()
+        {
+            ViewBag.Message = "Какой сегодня день? ->" + DateTime.Now;
+            return View();
+        }
+	}
+}
---
+++ ASP/Controllers/TwoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.Controllers
+{
+    public class TwoController : Controller
+    {
+        //
+        // GET: /Two/
+        public ActionResult View1()
+        {
+            ViewBag.Message = "Какое прекрасное утро! Не правда ли?";
+            return View();
+        }
+	}
+}
